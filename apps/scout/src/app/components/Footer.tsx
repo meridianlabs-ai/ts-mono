meridianlabs-ai/ts-mono-test

Initial commit - with sample apps/scout and packages/common

apps/scout/src/app/components/Footer.tsx
@@ -1,0 +1,118 @@
+import { formatNumber } from "@tsmono/common";
+import clsx from "clsx";
+import { FC, ReactNode } from "react";
+
+
+import styles from "./Footer.module.css";
+import { Pager } from "./Pager";
+
+interface FooterProps {
+  id: string;
+  className?: string | string[];
+
+  // Items
+  itemCount?: number;
+  filteredCount?: number;
+
+  // left side controls (replaced by progress when there is progress)
+  left?: ReactNode;
+
+  // Progress
+  progressText?: string;
+  progressBar?: ReactNode;
+
+  // Pagination
+  paginated: boolean;
+  pagesize?: number;
+  page?: number;
+  itemsPerPage?: number;
+  setPage?: (page: number) => void;
+
+  // labels
+  labels?: {
+    singular: string;
+    plural: string;
+  };
+}
+
+export const Footer: FC<FooterProps> = ({
+  id,
+  className,
+  itemCount = 0,
+  paginated,
+  filteredCount,
+  left,
+  progressText,
+  progressBar,
+  page,
+  setPage,
+  itemsPerPage = -1,
+  labels = {
+    singular: "item",
+    plural: "items",
+  },
+}) => {
+  // Get filtered count from the store
+  const effectiveItemCount = filteredCount ?? itemCount;
+
+  // Compute the start and end items
+  const currentPage = page || 0;
+  const pageItemCount = Math.min(
+    itemsPerPage,
+    effectiveItemCount - currentPage * itemsPerPage
+  );
+  const startItem = effectiveItemCount > 0 ? currentPage * itemsPerPage + 1 : 0;
+  const endItem = startItem + pageItemCount - 1;
+
+  return (
+    <div
+      id={id}
+      className={clsx("text-size-smaller", styles.footer, className)}
+    >
+      <div className={clsx(styles.left)}>
+        {progressText ? (
+          <div className={clsx(styles.spinnerContainer)}>
+            <div
+              className={clsx("spinner-border", styles.spinner)}
+              role="status"
+            >
+              <span className={clsx("visually-hidden")}>{progressText}...</span>
+            </div>
+            <div className={clsx("text-style-secondary", styles.label)}>
+              {progressText}...
+            </div>
+          </div>
+        ) : (
+          left
+        )}
+      </div>
+      <div className={clsx(styles.center)}>
+        {paginated && (
+          <Pager
+            itemCount={effectiveItemCount}
+            itemsPerPage={itemsPerPage}
+            page={page}
+            setPage={setPage}
+          />
+        )}
+      </div>
+      <div className={clsx(styles.right)}>
+        {progressBar ? (
+          progressBar
+        ) : paginated ? (
+          <div>
+            {effectiveItemCount === 0
+              ? ""
+              : filteredCount !== undefined && filteredCount !== itemCount
+                ? `${startItem} - ${endItem} / ${effectiveItemCount} (${itemCount} total)`
+                : `${startItem} - ${endItem} / ${effectiveItemCount}`}
+          </div>
+        ) : effectiveItemCount === 1 ? (
+          `${formatNumber(effectiveItemCount)} ${labels.singular}`
+        ) : (
+          `${formatNumber(effectiveItemCount)} ${labels.plural}`
+        )}
+      </div>
+    </div>
+  );
+};

apps/scout/src/app/server/useAdjacentTranscriptIds.ts
@@ -1,0 +1,107 @@
+import { skipToken } from "@tanstack/react-query";
+import { SortingState } from "@tanstack/react-table";
+import { AsyncData, data, loading } from "@tsmono/common";
+import { useEffect, useMemo } from "react";
+
+import { Condition } from "../../query";
+import { TranscriptsResponse } from "../../types/api-types";
+
+import { useServerTranscriptsInfinite } from "./useServerTranscriptsInfinite";
+
+type Position = { pageIndex: number; itemIndex: number };
+
+/**
+ * Returns prev/next transcript IDs relative to the given ID.
+ *
+ * Note: prev is undefined when viewing the first loaded item.
+ * Reverse paging not yet supported - will need backend prev_cursor.
+ */
+export const useAdjacentTranscriptIds = (
+  id: string,
+  location?: string | null,
+  pageSize?: number,
+  filter?: Condition,
+  sorting?: SortingState
+): AsyncData<[string | undefined, string | undefined]> => {
+  const {
+    data: queryData,
+    error,
+    isLoading,
+    hasNextPage,
+    isFetchingNextPage,
+    fetchNextPage,
+  } = useServerTranscriptsInfinite(
+    location ? { location, pageSize, filter, sorting } : skipToken
+  );
+
+  const pages = queryData?.pages;
+  const position = useMemo(
+    () => (pages ? findPosition(pages, id) : null),
+    [pages, id]
+  );
+
+  const needsNextPage =
+    pages !== undefined &&
+    position !== null &&
+    isLastLoadedItem(pages, position) &&
+    hasNextPage &&
+    !isFetchingNextPage;
+
+  useEffect(() => {
+    if (needsNextPage) {
+      fetchNextPage().catch(console.error);
+    }
+  }, [needsNextPage, fetchNextPage]);
+
+  if (isLoading) {
+    return loading;
+  }
+
+  if (error) {
+    return { loading: false, error };
+  }
+
+  if (pages === undefined || position === null) {
+    // ID not found in loaded pages - might need more data
+    return loading;
+  }
+
+  return data(getAdjacentIds(pages, position));
+};
+
+const findPosition = (
+  pages: TranscriptsResponse[],
+  id: string
+): Position | null => {
+  for (let p = 0; p < pages.length; p++) {
+    const page = pages[p];
+    if (!page) continue;
+    const i = page.items.findIndex((item) => item.transcript_id === id);
+    if (i >= 0) return { pageIndex: p, itemIndex: i };
+  }
+  return null;
+};
+
+const getAdjacentIds = (
+  pages: TranscriptsResponse[],
+  pos: Position
+): [string | undefined, string | undefined] => {
+  const page = pages[pos.pageIndex]!;
+  // Note: prevId is undefined at position 0; reverse paging not yet supported
+  const prevId =
+    pos.itemIndex > 0
+      ? page.items[pos.itemIndex - 1]?.transcript_id
+      : pages[pos.pageIndex - 1]?.items.at(-1)?.transcript_id;
+  const nextId =
+    pos.itemIndex < page.items.length - 1
+      ? page.items[pos.itemIndex + 1]?.transcript_id
+      : pages[pos.pageIndex + 1]?.items[0]?.transcript_id;
+  return [prevId, nextId];
+};
+
+const isLastLoadedItem = (
+  pages: TranscriptsResponse[],
+  pos: Position
+): boolean =>
+  pos.pageIndex === pages.length - 1 &&
+  pos.itemIndex === pages[pos.pageIndex]!.items.length - 1;

apps/scout/src/components/transcript/types.ts
@@ -1,0 +1,107 @@
+import {
+  ApprovalEvent,
+  CompactionEvent,
+  JsonChange,
+  ErrorEvent,
+  InfoEvent,
+  InputEvent,
+  LoggerEvent,
+  ModelEvent,
+  SampleInitEvent,
+  SampleLimitEvent,
+  SandboxEvent,
+  ScoreEvent,
+  ScoreEditEvent,
+  SpanBeginEvent,
+  SpanEndEvent,
+  StateEvent,
+  StepEvent,
+  StoreEvent,
+  SubtaskEvent,
+  ToolEvent,
+} from "../../types/api-types";
+
+import { STEP, SPAN_BEGIN, TYPE_TOOL, TYPE_SUBTASK } from "./transform/utils";
+
+export interface StateManager {
+  scope: string;
+  getState(): object;
+  initializeState(state: object): void;
+  applyChanges(changes: JsonChange[]): object;
+}
+
+export const kTranscriptCollapseScope = "transcript-collapse";
+export const kTranscriptOutlineCollapseScope = "transcript-outline";
+
+export const kCollapsibleEventTypes = [
+  STEP,
+  SPAN_BEGIN,
+  TYPE_TOOL,
+  TYPE_SUBTASK,
+];
+
+export type EventType =
+  | SampleInitEvent
+  | SampleLimitEvent
+  | StateEvent
+  | StoreEvent
+  | ModelEvent
+  | LoggerEvent
+  | InfoEvent
+  | StepEvent
+  | SubtaskEvent
+  | ScoreEvent
+  | ScoreEditEvent
+  | ToolEvent
+  | InputEvent
+  | ErrorEvent
+  | ApprovalEvent
+  | CompactionEvent
+  | SandboxEvent
+  | SpanBeginEvent
+  | SpanEndEvent;
+
+// Define the runtime array of all event type values
+export const eventTypeValues = [
+  "sample_init",
+  "sample_limit",
+  "state",
+  "store",
+  "model",
+  "logger",
+  "info",
+  "step",
+  "subtask",
+  "score",
+  "score_edit",
+  "tool",
+  "input",
+  "error",
+  "approval",
+  "compaction",
+  "sandbox",
+  "span_begin",
+  "span_end",
+] as const;
+
+// Derive the type from the array (replaces the indexed access approach)
+export type EventTypeValue = (typeof eventTypeValues)[number];
+export class EventNode<T extends EventType = EventType> {
+  id: string;
+  event: T;
+  children: EventNode<EventType>[] = [];
+  depth: number;
+
+  constructor(id: string, event: T, depth: number) {
+    this.id = id;
+    this.event = event;
+    this.depth = depth;
+  }
+}
+
+export interface TranscriptEventState {
+  selectedNav?: string;
+  collapsed?: boolean;
+}
+
+export type TranscriptState = Record<string, TranscriptEventState>;

meridianlabs-ai/ts-mono

apps/scout/src/app/components/Footer.tsx
@@ -1,0 +1,118 @@
+import clsx from "clsx";
+import { FC, ReactNode } from "react";
+
+import { formatNumber } from "@tsmono/util";
+
+import styles from "./Footer.module.css";
+import { Pager } from "./Pager";
+
+interface FooterProps {
+  id: string;
+  className?: string | string[];
+
+  // Items
+  itemCount?: number;
+  filteredCount?: number;
+
+  // left side controls (replaced by progress when there is progress)
+  left?: ReactNode;
+
+  // Progress
+  progressText?: string;
+  progressBar?: ReactNode;
+
+  // Pagination
+  paginated: boolean;
+  pagesize?: number;
+  page?: number;
+  itemsPerPage?: number;
+  setPage?: (page: number) => void;
+
+  // labels
+  labels?: {
+    singular: string;
+    plural: string;
+  };
+}
+
+export const Footer: FC<FooterProps> = ({
+  id,
+  className,
+  itemCount = 0,
+  paginated,
+  filteredCount,
+  left,
+  progressText,
+  progressBar,
+  page,
+  setPage,
+  itemsPerPage = -1,
+  labels = {
+    singular: "item",
+    plural: "items",
+  },
+}) => {
+  // Get filtered count from the store
+  const effectiveItemCount = filteredCount ?? itemCount;
+
+  // Compute the start and end items
+  const currentPage = page || 0;
+  const pageItemCount = Math.min(
+    itemsPerPage,
+    effectiveItemCount - currentPage * itemsPerPage
+  );
+  const startItem = effectiveItemCount > 0 ? currentPage * itemsPerPage + 1 : 0;
+  const endItem = startItem + pageItemCount - 1;
+
+  return (
+    <div
+      id={id}
+      className={clsx("text-size-smaller", styles.footer, className)}
+    >
+      <div className={clsx(styles.left)}>
+        {progressText ? (
+          <div className={clsx(styles.spinnerContainer)}>
+            <div
+              className={clsx("spinner-border", styles.spinner)}
+              role="status"
+            >
+              <span className={clsx("visually-hidden")}>{progressText}...</span>
+            </div>
+            <div className={clsx("text-style-secondary", styles.label)}>
+              {progressText}...
+            </div>
+          </div>
+        ) : (
+          left
+        )}
+      </div>
+      <div className={clsx(styles.center)}>
+        {paginated && (
+          <Pager
+            itemCount={effectiveItemCount}
+            itemsPerPage={itemsPerPage}
+            page={page}
+            setPage={setPage}
+          />
+        )}
+      </div>
+      <div className={clsx(styles.right)}>
+        {progressBar ? (
+          progressBar
+        ) : paginated ? (
+          <div>
+            {effectiveItemCount === 0
+              ? ""
+              : filteredCount !== undefined && filteredCount !== itemCount
+                ? `${startItem} - ${endItem} / ${effectiveItemCount} (${itemCount} total)`
+                : `${startItem} - ${endItem} / ${effectiveItemCount}`}
+          </div>
+        ) : effectiveItemCount === 1 ? (
+          `${formatNumber(effectiveItemCount)} ${labels.singular}`
+        ) : (
+          `${formatNumber(effectiveItemCount)} ${labels.plural}`
+        )}
+      </div>
+    </div>
+  );
+};

apps/scout/src/app/server/useAdjacentTranscriptIds.ts
@@ -1,0 +1,107 @@
+import { skipToken } from "@tanstack/react-query";
+import { SortingState } from "@tanstack/react-table";
+import { useEffect, useMemo } from "react";
+
+import { AsyncData, data, loading } from "@tsmono/util";
+
+import { Condition } from "../../query";
+import { TranscriptsResponse } from "../../types/api-types";
+import { useServerTranscriptsInfinite } from "./useServerTranscriptsInfinite";
+
+type Position = { pageIndex: number; itemIndex: number };
+
+/**
+ * Returns prev/next transcript IDs relative to the given ID.
+ *
+ * Note: prev is undefined when viewing the first loaded item.
+ * Reverse paging not yet supported - will need backend prev_cursor.
+ */
+export const useAdjacentTranscriptIds = (
+  id: string,
+  location?: string | null,
+  pageSize?: number,
+  filter?: Condition,
+  sorting?: SortingState
+): AsyncData<[string | undefined, string | undefined]> => {
+  const {
+    data: queryData,
+    error,
+    isLoading,
+    hasNextPage,
+    isFetchingNextPage,
+    fetchNextPage,
+  } = useServerTranscriptsInfinite(
+    location ? { location, pageSize, filter, sorting } : skipToken
+  );
+
+  const pages = queryData?.pages;
+  const position = useMemo(
+    () => (pages ? findPosition(pages, id) : null),
+    [pages, id]
+  );
+
+  const needsNextPage =
+    pages !== undefined &&
+    position !== null &&
+    isLastLoadedItem(pages, position) &&
+    hasNextPage &&
+    !isFetchingNextPage;
+
+  useEffect(() => {
+    if (needsNextPage) {
+      fetchNextPage().catch(console.error);
+    }
+  }, [needsNextPage, fetchNextPage]);
+
+  if (isLoading) {
+    return loading;
+  }
+
+  if (error) {
+    return { loading: false, error };
+  }
+
+  if (pages === undefined || position === null) {
+    // ID not found in loaded pages - might need more data
+    return loading;
+  }
+
+  return data(getAdjacentIds(pages, position));
+};
+
+const findPosition = (
+  pages: TranscriptsResponse[],
+  id: string
+): Position | null => {
+  for (let p = 0; p < pages.length; p++) {
+    const page = pages[p];
+    if (!page) continue;
+    const i = page.items.findIndex((item) => item.transcript_id === id);
+    if (i >= 0) return { pageIndex: p, itemIndex: i };
+  }
+  return null;
+};
+
+const getAdjacentIds = (
+  pages: TranscriptsResponse[],
+  pos: Position
+): [string | undefined, string | undefined] => {
+  const page = pages[pos.pageIndex]!;
+  // Note: prevId is undefined at position 0; reverse paging not yet supported
+  const prevId =
+    pos.itemIndex > 0
+      ? page.items[pos.itemIndex - 1]?.transcript_id
+      : pages[pos.pageIndex - 1]?.items.at(-1)?.transcript_id;
+  const nextId =
+    pos.itemIndex < page.items.length - 1
+      ? page.items[pos.itemIndex + 1]?.transcript_id
+      : pages[pos.pageIndex + 1]?.items[0]?.transcript_id;
+  return [prevId, nextId];
+};
+
+const isLastLoadedItem = (
+  pages: TranscriptsResponse[],
+  pos: Position
+): boolean =>
+  pos.pageIndex === pages.length - 1 &&
+  pos.itemIndex === pages[pos.pageIndex]!.items.length - 1;

apps/scout/src/components/transcript/types.ts
@@ -1,0 +1,106 @@
+import {
+  ApprovalEvent,
+  CompactionEvent,
+  ErrorEvent,
+  InfoEvent,
+  InputEvent,
+  JsonChange,
+  LoggerEvent,
+  ModelEvent,
+  SampleInitEvent,
+  SampleLimitEvent,
+  SandboxEvent,
+  ScoreEditEvent,
+  ScoreEvent,
+  SpanBeginEvent,
+  SpanEndEvent,
+  StateEvent,
+  StepEvent,
+  StoreEvent,
+  SubtaskEvent,
+  ToolEvent,
+} from "../../types/api-types";
+import { SPAN_BEGIN, STEP, TYPE_SUBTASK, TYPE_TOOL } from "./transform/utils";
+
+export interface StateManager {
+  scope: string;
+  getState(): object;
+  initializeState(state: object): void;
+  applyChanges(changes: JsonChange[]): object;
+}
+
+export const kTranscriptCollapseScope = "transcript-collapse";
+export const kTranscriptOutlineCollapseScope = "transcript-outline";
+
+export const kCollapsibleEventTypes = [
+  STEP,
+  SPAN_BEGIN,
+  TYPE_TOOL,
+  TYPE_SUBTASK,
+];
+
+export type EventType =
+  | SampleInitEvent
+  | SampleLimitEvent
+  | StateEvent
+  | StoreEvent
+  | ModelEvent
+  | LoggerEvent
+  | InfoEvent
+  | StepEvent
+  | SubtaskEvent
+  | ScoreEvent
+  | ScoreEditEvent
+  | ToolEvent
+  | InputEvent
+  | ErrorEvent
+  | ApprovalEvent
+  | CompactionEvent
+  | SandboxEvent
+  | SpanBeginEvent
+  | SpanEndEvent;
+
+// Define the runtime array of all event type values
+export const eventTypeValues = [
+  "sample_init",
+  "sample_limit",
+  "state",
+  "store",
+  "model",
+  "logger",
+  "info",
+  "step",
+  "subtask",
+  "score",
+  "score_edit",
+  "tool",
+  "input",
+  "error",
+  "approval",
+  "compaction",
+  "sandbox",
+  "span_begin",
+  "span_end",
+] as const;
+
+// Derive the type from the array (replaces the indexed access approach)
+export type EventTypeValue = (typeof eventTypeValues)[number];
+export class EventNode<T extends EventType = EventType> {
+  id: string;
+  event: T;
+  children: EventNode<EventType>[] = [];
+  depth: number;
+
+  constructor(id: string, event: T, depth: number) {
+    this.id = id;
+    this.event = event;
+    this.depth = depth;
+  }
+}
+
+export interface TranscriptEventState {
+  selectedNav?: string;
+  collapsed?: boolean;
+}
+
+export type TranscriptState = Record<string, TranscriptEventState>;