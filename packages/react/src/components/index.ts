20a36cc09ee7196b797c15e975c8d1261886cf86
Charles Teague <charles@meridianlabs.ai>
2026-03-27 13:54:13 -0400

    Migrate ConfirmationDialog

--- packages/react/src/components/index.ts
+++ packages/react/src/components/index.ts
@@ -1,5 +1,6 @@
 export * from "./AnsiDisplay";
 export * from "./AsciinemaPlayer";
+export * from "./ConfirmationDialog";
 export * from "./Modal";
 export * from "./StickyScroll";
 export * from "./ToolButton";
