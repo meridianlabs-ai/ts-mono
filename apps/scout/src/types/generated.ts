ed3b3f72fa8e25f5ef13cc388a73e961a7645f0e
J.J. Allaire <jj.allaire@gmail.com>
2026-04-27 10:33:20 -0400

    update scout types

--- apps/scout/src/types/generated.ts
+++ apps/scout/src/types/generated.ts
@@ -3336,6 +3336,11 @@ export interface components {
             /** Span Type */
             span_type?: string | null;
             /**
+             * Tool Invoked
+             * @default false
+             */
+            tool_invoked: boolean;
+            /**
              * Type
              * @default span
              * @constant
